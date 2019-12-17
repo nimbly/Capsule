e49761fabc267102c9e7fe58101073a212f55e54
Brent Scheffler <brent@brentscheffler.com>
2019-12-16 16:49:35 -0800

    Test coverage for UploadedFileFactory.

--- tests/UploadedFileFactoryTest.php
+++ tests/UploadedFileFactoryTest.php
@@ -3,15 +3,37 @@
 namespace Capsule\Tests;
 
 use Capsule\Factory\UploadedFileFactory;
+use Capsule\Stream\BufferStream;
 use Capsule\UploadedFile;
 use PHPUnit\Framework\TestCase;
 
 /**
  * @covers Capsule\Factory\UploadedFileFactory
  * @covers Capsule\UploadedFile
+ * @covers Capsule\Stream\BufferStream
  */
 class UploadedFileFactoryTest extends TestCase
 {
+	public function test_create_uploaded_file()
+	{
+		$stream = new BufferStream("Capsule!");
+
+		$uploadedFileFactory = new UploadedFileFactory;
+		$uploadedFile = $uploadedFileFactory->createUploadedFile(
+			$stream,
+			$stream->getSize(),
+			UPLOAD_ERR_OK,
+			"test.json",
+			"text/json"
+		);
+
+		$this->assertInstanceOf(UploadedFile::class, $uploadedFile);
+		$this->assertEquals("test.json", $uploadedFile->getClientFilename());
+		$this->assertEquals("text/json", $uploadedFile->getClientMediaType());
+		$this->assertEquals($stream->getSize(), $uploadedFile->getSize());
+		$this->assertEquals(UPLOAD_ERR_OK, $uploadedFile->getError());
+	}
+
 	public function test_create_from_global()
 	{
 		if( !\is_dir(__DIR__ . "/tmp") ){
