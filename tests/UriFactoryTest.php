fb37d2786463c2fc5a35d90d28ded16e5044bad8
Brent Scheffler <brent@brentscheffler.com>
2020-01-21 19:56:20 -0800

    Adding missing test coverage for UriFactory::createUri().

--- tests/UriFactoryTest.php
+++ tests/UriFactoryTest.php
@@ -11,6 +11,22 @@ use PHPUnit\Framework\TestCase;
  */
 class UriFactoryTest extends TestCase
 {
+	public function test_create_uri()
+	{
+		$url = "https://username:password@www.example.com:443/path/to/some/resource?q=foo&s=some+search+text&n=John%20Doe#fragment-1";
+		$uriFactory = new UriFactory;
+		$uri = $uriFactory->createUri($url);
+
+		$this->assertEquals("https", $uri->getScheme());
+        $this->assertEquals("username:password", $uri->getUserInfo());
+        $this->assertEquals("www.example.com", $uri->getHost());
+        $this->assertEquals(443, $uri->getPort());
+        $this->assertEquals("/path/to/some/resource", $uri->getPath());
+        $this->assertEquals("q=foo&s=some+search+text&n=John%20Doe", $uri->getQuery());
+        $this->assertEquals("fragment-1", $uri->getFragment());
+        $this->assertEquals("username:password@www.example.com:443", $uri->getAuthority());
+	}
+
 	public function test_make_from_string_parses_all_uri_parts()
     {
         $url = "https://username:password@www.example.com:443/path/to/some/resource?q=foo&s=some+search+text&n=John%20Doe#fragment-1";
